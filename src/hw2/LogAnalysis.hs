639fb9fb13e458c8b18279699376f136a2e49e64
huyngo1216 <huyngo1216@gmail.com>
2021-04-29 16:00:00 -0700

    Finish Exercise 4: inOrder

--- src/hw2/LogAnalysis.hs
+++ src/hw2/LogAnalysis.hs
@@ -13,3 +13,23 @@ parseMessage s = case words s of
 
 parse :: String -> [LogMessage]
 parse contents = fmap parseMessage (lines contents)
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert m@(LogMessage _ t _) tree = case tree of
+        Leaf -> Node Leaf m Leaf
+        Node Leaf m'@(LogMessage _ t' _) Leaf -> if t > t' then Node Leaf m' (insert m Leaf) else Node (insert m Leaf) m' Leaf
+        Node Leaf m'@(LogMessage _ t' _) tree'@(Node _ _ _) -> if t > t' then Node Leaf m' (insert m tree') else Node (Node Leaf m Leaf) m' tree'
+        Node tree'@(Node _ _ _) m'@(LogMessage _ t' _) Leaf -> if t > t' then Node tree' m' (Node Leaf m Leaf) else insert m tree'
+        Node tree'@(Node _ _ _) m'@(LogMessage _ t' _) tree''@(Node _ _ _) -> if t > t' then Node tree' m' (insert m tree'') else Node (insert m tree') m' tree''
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder n = case n of
+        Node Leaf m Leaf -> [m]
+        Node Leaf m tree'@(Node _ _ _) -> [m] ++ inOrder tree'
+        Node tree'@(Node _ _ _) m Leaf ->  inOrder tree' ++ [m]
+        Node tree'@(Node _ _ _) m tree''@(Node _ _ _) -> inOrder tree' ++ [m] ++ inOrder tree''
